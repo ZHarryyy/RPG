0e9215c97675e7955aac007a8319601eef7b6aa5
Harry <zharryyyyy@gmail.com>
2024-01-13 16:54:13 +0800

    角色基础移动+动画

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,5 +2,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private float xInput;
+
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Movement();
+        CheckInput();
+        FlipController();
+        AnimatorControllers();
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if(rb.velocity.x > 0 && !facingRight) Flip();
+        else if(rb.velocity.x < 0 && facingRight) Flip();
+    }
 }

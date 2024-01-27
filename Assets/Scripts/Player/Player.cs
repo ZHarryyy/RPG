0d6f6d0ed9709137530779a032714ccf46d2c58c
ZHarryyy <49617057+ZHarryyy@users.noreply.github.com>
2024-01-27 19:04:29 +0800

    瞄准+投掷+回收剑状态优化

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,6 +12,7 @@ public class Player : Entity
     [Header("Move info")]
     public float moveSpeed = 12f;
     public float jumpForce;
+    public float swordReturnImpact;
 
     [Header("Dash info")]
     public float dashSpeed;
@@ -19,7 +20,7 @@ public class Player : Entity
     public float dashDir { get; private set; }
 
     public SkillManager skill { get; private set; }
-    public GameObject sword;// { get; private set; }
+    public GameObject sword { get; private set; }
 
     #region States
     public PlayerStateMachine stateMachine { get; private set; }
@@ -83,8 +84,9 @@ public class Player : Entity
         sword = _newSword;
     }
 
-    public void ClearTheSword()
+    public void CatchTheSword()
     {
+        stateMachine.ChangeState(catchSwordState);
         Destroy(sword);
     }
 
--- Assets/Scripts/Player/PlayerAimSwordState.cs
+++ Assets/Scripts/Player/PlayerAimSwordState.cs
@@ -17,11 +17,20 @@ public class PlayerAimSwordState : PlayerState
     {
         base.Update();
 
+        player.SetZeroVelocity();
+
         if(Input.GetKeyUp(KeyCode.Mouse1)) stateMachine.ChangeState(player.idleState);
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(player.transform.position.x > mousePosition.x && player.facingDir == 1) player.Flip();
+        else if(player.transform.position.x < mousePosition.x && player.facingDir == -1) player.Flip();
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        player.StartCoroutine("BusyFor", .2f);
     }
 }

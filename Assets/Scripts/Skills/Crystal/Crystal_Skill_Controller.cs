b3d435b7396a572a9c157448daca3c3c1102d5ed
ZHarryyy <49617057+ZHarryyy@users.noreply.github.com>
2024-01-31 18:05:30 +0800

    爆炸水晶技能

---
+++ Assets/Scripts/Skills/Crystal/Crystal_Skill_Controller.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Crystal_Skill_Controller : MonoBehaviour
+{
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
+
+    private float crystalExistTimer;
+
+    private bool canExplode;
+    private bool canMoveToEnemy;
+    private float moveSpeed;
+
+    private bool canGrow;
+    private float growSpeed = 5;
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMoveToEnemy, float _moveSpeed)
+    {
+        crystalExistTimer = _crystalDuration;
+        canExplode = _canExplode;
+        canMoveToEnemy = _canMoveToEnemy;
+        moveSpeed = _moveSpeed;
+    }
+
+    private void Update()
+    {
+        crystalExistTimer -= Time.deltaTime;
+
+        if(crystalExistTimer < 0) FinishCrystal();
+
+        if(canGrow) transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+    }
+
+    private void AnimationExplodeEvent()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach(var hit in colliders)
+        {
+            if(hit.GetComponent<Enemy>() != null) hit.GetComponent<Enemy>().Damage();
+        }
+    }
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        }
+        else SelfDestroy();
+    }
+
+    public void SelfDestroy() => Destroy(gameObject);
+}

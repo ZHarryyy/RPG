a9ba92bf79d630a48246cf2bd975aa0f0ad34b28
ZHarryyy <49617057+ZHarryyy@users.noreply.github.com>
2024-02-04 18:13:18 +0800

    防御属性+魔法属性+暴击+魔法伤害

--- Assets/Scripts/Stat/CharacterStats.cs
+++ Assets/Scripts/Stat/CharacterStats.cs
@@ -2,22 +2,87 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("Major stats")]
     public Stat strength;
+    public Stat agility;
+    public Stat intelligence;
+    public Stat vitality;
+
+    [Header("Offensive stats")]
     public Stat damage;
+    public Stat critChance;
+    public Stat critPower;
+
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+    public Stat magicResistance;
+
+    [Header("Magic stats")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat lightingDamage;
+
+    public bool isIgnited;
+    public bool isChilled;
+    public bool isShocked;
 
     [SerializeField] private int currentHealth;
 
     protected virtual void Start()
     {
+        critPower.SetDefaultValue(150);   
         currentHealth = maxHealth.GetValue();
     }
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats)) return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
 
-        _targetStats.TakeDamage(totalDamage);
+        if(CanCrit())
+        {
+            totalDamage = CalculateCriticalDamage(totalDamage);
+        }
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
+
+        // _targetStats.TakeDamage(totalDamage);
+        DoMagicDamage(_targetStats);
+    }
+
+    public virtual void DoMagicDamage(CharacterStats _targetStats)
+    {
+        int _fireDamage = fireDamage.GetValue();
+        int _iceDamage = iceDamage.GetValue();
+        int _lightingDamage = lightingDamage.GetValue();
+
+        int totalMagicalDamage = _fireDamage + _iceDamage + _lightingDamage + intelligence.GetValue();
+
+        totalMagicalDamage = CheckTargetResistance(_targetStats, totalMagicalDamage);
+        _targetStats.TakeDamage(totalMagicalDamage);
+    }
+
+    private static int CheckTargetResistance(CharacterStats _targetStats, int totalMagicalDamage)
+    {
+        totalMagicalDamage -= _targetStats.magicResistance.GetValue() + (_targetStats.intelligence.GetValue() * 3);
+        totalMagicalDamage = Mathf.Clamp(totalMagicalDamage, 0, int.MaxValue);
+        return totalMagicalDamage;
+    }
+
+    public void ApplyAliments(bool _ignite, bool _chill, bool _shock)
+    {
+        if(isIgnited || isChilled || isShocked)
+        {
+            return;
+        }
+
+        isIgnited = _ignite;
+        isChilled = _chill;
+        isShocked = _shock;
     }
 
     public virtual void TakeDamage(int _damage)
@@ -32,5 +97,45 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CanCrit()
+    {
+        int totalCriticalChance = critChance.GetValue() + agility.GetValue();
+
+        if(Random.Range(0, 100) <= totalCriticalChance)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int CalculateCriticalDamage(int _damage)
+    {
+        float totalCritPower = (critPower.GetValue() + strength.GetValue()) * .01f;
+
+        float critDamage = _damage * totalCritPower;
+
+        return Mathf.RoundToInt(critDamage);
     }
 }

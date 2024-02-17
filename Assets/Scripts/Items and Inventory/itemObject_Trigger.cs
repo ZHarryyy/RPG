34e6bf2d1b17244ca4429ab2b5b65a8cb4997bbc
ZHarryyy <49617057+ZHarryyy@users.noreply.github.com>
2024-02-17 18:04:50 +0800

    道具掉落

---
+++ Assets/Scripts/Items and Inventory/itemObject_Trigger.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class itemObject_Trigger : MonoBehaviour
+{
+    private ItemObject myItemObject => GetComponentInParent<ItemObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null) myItemObject.PickupItem();
+    }
+}
